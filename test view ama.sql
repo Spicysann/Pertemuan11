5dc8a7cf8f26af784d38e0eeb9c8b86d8e65b04f
amaliyah nur haida <107660930+amaliyahhaida@users.noreply.github.com>
2024-12-11 19:30:21 +0700

    Delete test view ama.sql

--- test view ama.sql
+++
@@ -1,5 +0,0 @@
-CREATE VIEW amaa1 AS
-
-SELECT Reservations.room_id, Services.reservation_id
-FROM     Reservations INNER JOIN
-                  Services ON Reservations.reservation_id = Services.reservation_id
