c5cc460e581bbd6e30aa50d36d671c123383b205
Spycisann <sandyalfaizal49@gmail.com>
2024-12-11 21:30:52 +0700

    view 1 dan 2

--- view 1.sql
+++ view 1.sql
@@ -1,4 +1,13 @@
-SELECT Departments.DepartmentsID, Departments.DepartmentName, Employee.EmployeeID, Employee.Name, Salaries.EmployeeID AS Expr1, Salaries.Salaries
-FROM   Departments INNER JOIN
-             Employee ON Departments.DepartmentsID = Employee.DepartmentsID INNER JOIN
-             Salaries ON Employee.EmployeeID = Salaries.EmployeeID
+CREATE VIEW Tugas AS
+SELECT 
+    Departments.DepartmentsID,
+    Departments.DepartmentName,
+    Employee.EmployeeID,
+    Employee.Name AS EmployeeName,
+    Salaries
+FROM 
+    Departments
+INNER JOIN 
+    Employee ON Departments.DepartmentsID = Employee.DepartmentsID
+INNER JOIN 
+    Salaries ON Employee.EmployeeID = Salaries.EmployeeID;
