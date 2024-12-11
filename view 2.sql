a6c7256c09d75a3a13a92232796c97272033b568
Spicysann <145399553+Spicysann@users.noreply.github.com>
2024-12-11 21:29:45 +0700

    Delete view 2.sql

--- view 2.sql
+++
@@ -1,4 +0,0 @@
-SELECT Employee.EmployeeID, Employee.Name, Rooms.room_id, Rooms.room_type, Rooms.price, Rooms.status, Salaries.EmployeeID AS Expr1, Salaries.Salaries
-FROM   Employee INNER JOIN
-             Salaries ON Employee.EmployeeID = Salaries.EmployeeID CROSS JOIN
-             Rooms
