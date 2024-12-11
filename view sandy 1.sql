9d9eca163fb66d58122207789fc2284452adfd71
Spicysann <145399553+Spicysann@users.noreply.github.com>
2024-12-11 20:09:14 +0700

    Delete view sandy 1.sql

--- view sandy 1.sql
+++
@@ -1,3 +0,0 @@
-SELECT dpp_mahasiswa.dpp_id, dpp_mahasiswa.mahasiswa_id, dpp_mahasiswa.jumlah_pembayaran, dpp_mahasiswa.tanggal_pembayaran, mahasiswa.mahasiswa_id AS Expr1, mahasiswa.nama_mahasiswa, mahasiswa.nim, mahasiswa.tanggal_lahir
-FROM   dpp_mahasiswa INNER JOIN
-             mahasiswa ON dpp_mahasiswa.mahasiswa_id = mahasiswa.mahasiswa_id
