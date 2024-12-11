b6c379d37ad956bdf9d7fa1f3acb49720531bfc2
dwiyyl <144828638+dwiyyl@users.noreply.github.com>
2024-12-11 14:58:08 +0700

    Delete coba wi1.sql

--- coba wi1.sql
+++
@@ -1,7 +0,0 @@
-use TUGAS_PBDNO8;
-
-SELECT FROM   Rooms INNER JOIN
-             Rooms AS Rooms_1 ON Rooms.room_id = Rooms_1.room_id INNER JOIN
-             Rooms AS Rooms_2 ON Rooms.room_id = Rooms_2.room_id CROSS JOIN
-             Customers CROSS JOIN
-             Payments
