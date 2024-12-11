38e3e7fe53a7a8b715d4f2f4f8bee08ff79fe51e
Spicysann <145399553+Spicysann@users.noreply.github.com>
2024-12-11 21:29:35 +0700

    Delete view 1.sql

--- view 1.sql
+++
@@ -1,4 +0,0 @@
-SELECT Departments.DepartmentsID, Departments.DepartmentName, Employee.EmployeeID, Employee.Name, Salaries.EmployeeID AS Expr1, Salaries.Salaries
-FROM   Departments INNER JOIN
-             Employee ON Departments.DepartmentsID = Employee.DepartmentsID INNER JOIN
-             Salaries ON Employee.EmployeeID = Salaries.EmployeeID
